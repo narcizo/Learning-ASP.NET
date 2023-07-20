4fd20da08564e7032c2696a65a1ca7cf5705f825
narcizo <narcizo.gabriel2@gmail.com>
2023-07-19 22:15:36 -0300

    Model for new entity Book created

---
+++ GenericProject/GenericProject/Model/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GenericProject.Model
+{
+
+    [Table("books")]
+    public class Book
+    {
+        [Column("id")]
+        private long Id;
+
+        [Column("author")]
+        private string Author;
+
+        [Column("title")]
+        private string Title;
+
+        [Column("launch_date")]
+        private string LauchDate;
+
+        [Column("price")]
+        private string Price;
+
+    }
+}
